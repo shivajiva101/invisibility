61c9d532d6c2ab7ac686369aa66e8e724833e67f
Panquesito7 <51391473+Panquesito7@users.noreply.github.com>
2019-06-10 16:16:16 -0500

    Fix collisionbox when turning off invisible (#1)
    * Fix collision box (for 5.0.0+ only).
    
    This commit fixes the error making the player's collisionbox bigger.
    
    * Update init.lua
    
    * Create mod.conf
    
    * Update mod.conf
    
    * Create description.txt

--- init.lua
+++ init.lua
@@ -41,7 +41,7 @@ invisible = function(player, toggle)
 		-- show player and tag
 		prop = {
 			visual_size = {x = 1, y = 1},
-			collisionbox = {-0.35, -1, -0.35, 0.35, 1, 0.35}
+			collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3}
 		}
 
 		player:set_nametag_attributes({
@@ -86,3 +86,8 @@ minetest.register_chatcommand("vanish", {
 
 	end
 })
+
+-- Log
+if minetest.settings:get_bool("log_mods") then
+minetest.log("action", "[MOD] Invisibility loaded")
+end		
